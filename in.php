0e9e2de3822032b30364589f1eb3d183185b0dc0
Kerzana <33467099+Kerzana@users.noreply.github.com>
2018-04-24 22:01:33 +0200

    DDD
    dddd

---
+++ in.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+        Stats Fornite
+        </title>
+        <meta charset="UTF-8"/>
+    </head>
+    <body>
+        <?php
+            $curl        = curl_init();
+            $playerName  = $_GET['username'];
+			$plat  		 = $_GET['platform']; 
+            $apiKey      = "gtl12fdZRfs1uX7DF9gA";
+
+            $encoded_url = "https://fortnite.y3n.co/v2/player/" . $playerName;
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array("X-Key: " . $apiKey));
+            curl_setopt($curl, CURLOPT_URL, $encoded_url);
+            curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+            $result     =  json_decode(curl_exec($curl), true);
+            
+            curl_close($curl);
+			echo "<pre>";
+			print_r($result['br']['stats'][$plat]);
+			echo "</pre>"
+        ?>
+    </body>
+</html>
